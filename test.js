d359fc7852a3b77757bfdfa96ed2d059a6fb7a1b
1210960058 <1210960058u@hiro.kindai.ac.jp>
2015-04-23 17:30:01 +0900

    名前とメッセージが送信可能

--- test.js
+++ test.js
@@ -6,15 +6,21 @@ var setting = require('./setting');//setting.jsと言う外部ファイルを読
 var server = http.createServer();//サーバーオブジェクトの作成
 var template = fs.readFileSync(__dirname + '/bbs.ejs','utf-8');
 var posts = [];
-function renderForm(posts,res){
+var massage = [];
+function renderForm(posts,massage,res){
 	var date = ejs.render(template,{
-		posts: posts
+		posts: posts,
+		massage: massage
 	});
 	res.writeHead(200,{'Content-Type' : 'text/html'});
 	res.write(date);
 	res.end();
 }
-server.on('request',function(req, res){
+server.on('request',doRequest);
+server.listen(setting.port,setting.host);//settingに書かれたポートとホストで待ち
+console.log("server listning/////")//待ち状態を表すコンソールログ
+
+function doRequest(req, res){
 	if(req.method === 'POST'){
 		req.date = "";
 		req.on("readable",function(){
@@ -23,15 +29,15 @@ server.on('request',function(req, res){
 		req.on("end",function(){
 			var query = qs.parse(req.date);
 			posts.push(query.name);
-			console.log(query.name)
-			renderForm(posts, res);
+			console.log(query.name);
+			massage.push(query.message);
+			console.log(query.message);
+			renderForm(posts,massage, res);
 
 		});
 	} else{
-		renderForm(posts,res);
+		renderForm(posts,massage,res);
 	}
 
 
-});
-server.listen(setting.port,setting.host);//settingに書かれたポートとホストで待ち
-console.log("server listning/////")//待ち状態を表すコンソールログ
+}
